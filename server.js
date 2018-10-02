50eab23132b71bcd236a70ed4bc0125557c58cf4
Yehor Petruk <yehor@juniqe.com>
2018-10-02 15:20:22 +0200

    Add portfolio page

--- server.js
+++ server.js
@@ -19,10 +19,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use((req, res, next) => {
-  res.render('maintenance.hbs');
-});
-
 app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
@@ -45,6 +41,12 @@ app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle: 'About Page'
   });
+});
+
+app.get('/portfolio', (req, res) => {
+  res.render('portfolio.hbs', {
+    pageTitle: 'Portfolio'
+  })
 });
 
 app.get('/bad', (req, res) => {
